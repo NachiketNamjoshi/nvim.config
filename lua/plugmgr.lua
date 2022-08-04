b5b001b3aa0b4871bd17d15c862cff8d00001402
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2022-08-04 17:07:27 +0530

    Handle plugmgr better
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- lua/plugmgr.lua
+++ lua/plugmgr.lua
@@ -1,16 +1,66 @@
+-- Stolen from :: https://raw.githubusercontent.com/ChristianChiarulli/nvim/master/lua/user/plugins.lua
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+---@diagnostic disable-next-line: missing-parameter
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  -- snapshot = "july-24",
+  snapshot_path = fn.stdpath "config" .. "/snapshots",
+  max_jobs = 50,
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+    prompt_border = "rounded", -- Border style of prompt popups.
+  },
+}
+
 return require('packer').startup(function()
 	-- packer
 	use 'wbthomason/packer.nvim'
-
-	use 'tpope/vim-sensible'
 
 	-- colorscheme
 	use 'gruvbox-community/gruvbox'
 
 	use 'nvim-treesitter/nvim-treesitter'
 
+	use 'neovim/nvim-lspconfig'
+
 	use {
 		'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
