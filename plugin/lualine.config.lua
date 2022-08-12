8e0ad77f0d310c06fec6ac1fffe9ccd5aa04f608
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2022-08-12 15:51:43 +0530

    Switch to statically compiled theme
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- plugin/lualine.config.lua
+++ plugin/lualine.config.lua
@@ -3,15 +3,28 @@ if not ok then
 	return
 end
 
+local lualine_opts = {
+	icons_enabled = true,
+	always_divide_middle = true,
+	refresh = {
+		statusline = 1000,
+		tabline = 1000,
+		winbar = 1000,
+	},
+  section_separators = { left = '', right = '' },
+  component_separators = { left = '', right = '' },
+}
+
+local reactor_ok, reactor = pcall(require, 'colors.reactor')
+if reactor_ok then
+  vim.notify("Loading Reactor Lualine Theme", vim.log.levels.INFO, {
+    title = "reactor colorscheme"
+  })
+  lualine_opts['theme'] = reactor.LualineTheme;
+else
+  lualine_opts['theme'] = 'gruvbox'
+end
+
 lualine.setup {
-	options = {
-		icons_enabled = true,
-		theme = 'gruvbox',
-		always_divide_middle = true,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	}
+	options = lualine_opts
 }
