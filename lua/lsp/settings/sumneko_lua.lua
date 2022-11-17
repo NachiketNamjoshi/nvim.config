9723170256940007418debd2972f2b7fe0110bd6
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2022-11-17 11:16:35 +0530

    Update luadev to neodev
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- lua/lsp/settings/sumneko_lua.lua
+++ lua/lsp/settings/sumneko_lua.lua
@@ -1,26 +1,30 @@
-local luadev_ok, luadev = pcall(require, 'neodev')
-if not luadev_ok then
+local neodev_ok, neodev = pcall(require, 'neodev')
+if neodev_ok then
+  neodev.setup({})
   return {
-	  settings = {
-		  Lua = {
-			  diagnostics = {
-				  globals = { "vim" },
-			  },
-			  workspace = {
-				  library = {
-					  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					  [vim.fn.stdpath("config") .. "/lua"] = true,
-				  },
-			  },
-		  },
-	  },
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = "Replace"
+        }
+      }
+    }
   }
 end
 
-return luadev.setup({
-  library = {
-    vimruntime = true, -- runtime path
-    types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-    plugins = true, -- installed opt or start plugins in packpath
-  }
-})
+
+return {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+}
