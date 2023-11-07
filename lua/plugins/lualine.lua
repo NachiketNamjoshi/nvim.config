c41a818be878ce5c32f71026eacae3c3930f9b96
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2023-11-07 12:23:42 +0530

    update lualine for lazy syntax
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,18 @@
   -- Status Line
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
-	opts = {
-		icons_enabled = true,
-		always_divide_middle = true,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-  	section_separators = { left = '', right = '' },
-  	component_separators = { left = '', right = '' },
-  	theme = "gruvbox"
-	}
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
+  event = 'BufEnter',
+  opts = {
+    icons_enabled = true,
+    always_divide_middle = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    theme = "gruvbox"
+  }
 }
