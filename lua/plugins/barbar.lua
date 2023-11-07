39ee6186f6462f7c46a373512d41514880ab3e21
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2023-11-07 12:23:30 +0530

    update barbar for lazy syntax
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,84 +1,90 @@
 return {
   'romgrk/barbar.nvim',
-  config = function()
+  dependencies = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    'lewis6991/gitsigns.nvim', -- optional, for git status
+  },
+  event = 'BufEnter',
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
     -- Set barbar options
-    require'bufferline'.setup {
-      -- Enable/disable animations
-      animation = true,
+    -- Enable/disable animations
+    animation = true,
 
-      -- Enable/disable auto-hiding the tab bar when there is a single buffer
-      auto_hide = false,
+    -- Enable/disable auto-hiding the tab bar when there is a single buffer
+    auto_hide = false,
 
-      -- Enable/disable current/total tabpages indicator (top right corner)
-      tabpages = true,
+    -- Enable/disable current/total tabpages indicator (top right corner)
+    tabpages = true,
 
-      -- Enable/disable close button
-      closable = true,
+    -- Enable/disable close button
+    closable = true,
 
-      -- Enables/disable clickable tabs
-      --  - left-click: go to buffer
-      --  - middle-click: delete buffer
-      clickable = true,
+    -- Enables/disable clickable tabs
+    --  - left-click: go to buffer
+    --  - middle-click: delete buffer
+    clickable = true,
 
-      -- Excludes buffers from the tabline
-      exclude_ft = {'javascript'},
-      exclude_name = {'package.json'},
+    -- Excludes buffers from the tabline
+    exclude_ft = {'javascript'},
+    exclude_name = {'package.json'},
 
-      -- Enable/disable icons
-      -- if set to 'numbers', will show buffer index in the tabline
-      -- if set to 'both', will show buffer index and icons in the tabline
-      icons = {
-        filetype = {
-          -- Sets the icon's highlight group.
-          -- If false, will use nvim-web-devicons colors
-          custom_colors = false,
+    -- Enable/disable icons
+    -- if set to 'numbers', will show buffer index in the tabline
+    -- if set to 'both', will show buffer index and icons in the tabline
+    icons = {
+      filetype = {
+        -- Sets the icon's highlight group.
+        -- If false, will use nvim-web-devicons colors
+        custom_colors = false,
 
-          -- Requires `nvim-web-devicons` if `true`
-          enabled = true,
-        },
-        separator = {
-          left = '',
-          right = ''
-        },
-        modified = {
-          button = '●'
-        },
-        pinned = {
-          button = '',
-          filename = true,
-          separator = {
-            right = '',
-            left = '',
-          }
-        },
-        button = '',
+        -- Requires `nvim-web-devicons` if `true`
+        enabled = true,
       },
+      separator = {
+        left = '',
+        right = ''
+      },
+      modified = {
+        button = '●'
+      },
+      pinned = {
+        button = '',
+        filename = true,
+        separator = {
+          right = '',
+          left = '',
+        }
+      },
+      button = '',
+    },
 
-      -- If true, new buffers will be inserted at the start/end of the list.
-      -- Default is to insert after current buffer.
-      insert_at_end = false,
-      insert_at_start = false,
+    -- If true, new buffers will be inserted at the start/end of the list.
+    -- Default is to insert after current buffer.
+    insert_at_end = false,
+    insert_at_start = false,
 
-      -- Sets the maximum padding width with which to surround each tab
-      maximum_padding = 1,
+    -- Sets the maximum padding width with which to surround each tab
+    maximum_padding = 1,
 
-      -- Sets the maximum buffer name length.
-      maximum_length = 30,
+    -- Sets the maximum buffer name length.
+    maximum_length = 30,
 
-      -- If set, the letters for each buffer in buffer-pick mode will be
-      -- assigned based on their name. Otherwise or in case all letters are
-      -- already assigned, the behavior is to assign letters in order of
-      -- usability (see order below)
-      semantic_letters = true,
+    -- If set, the letters for each buffer in buffer-pick mode will be
+    -- assigned based on their name. Otherwise or in case all letters are
+    -- already assigned, the behavior is to assign letters in order of
+    -- usability (see order below)
+    semantic_letters = true,
 
-      -- New buffer letters are assigned in this order. This order is
-      -- optimal for the qwerty keyboard layout but might need adjustement
-      -- for other layouts.
-      letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+    -- New buffer letters are assigned in this order. This order is
+    -- optimal for the qwerty keyboard layout but might need adjustement
+    -- for other layouts.
+    letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
 
-      -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
-      -- where X is the buffer number. But only a static string is accepted here.
-      no_name_title = nil,
-    }
-  end
+    -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
+    -- where X is the buffer number. But only a static string is accepted here.
+    no_name_title = nil,
+  }
 }
