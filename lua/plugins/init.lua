94f0e10107fd74fd9a45cc855c53350063cd9b44
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2023-11-08 16:01:11 +0530

    Add dressing.nvim for better inputs
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,12 +19,13 @@ return {
   "ziontee113/color-picker.nvim",
   'gruvbox-community/gruvbox',
   { 'NachiketNamjoshi/reactor.nvim', build = './install.sh' },
-
+  -- Better inputs
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+    event = "VimEnter",
+  },
 
   -- Notifications
   'rcarriga/nvim-notify',
-
-  -- Buffers as Tabs
-  'romgrk/barbar.nvim',
-
 }
